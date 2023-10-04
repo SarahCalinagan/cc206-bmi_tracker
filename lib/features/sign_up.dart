1cbdb26936fdc71e12cbac00d4f7fd919695c2b8
Fatima Grace Apinan <89889380+FreyaGrace@users.noreply.github.com>
2023-10-05 02:59:52 +0800

    added sign-up

--- lib/features/sign_up.dart
+++ lib/features/sign_up.dart
@@ -1,2 +1,60 @@
-//Hello
-//hello
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+    
+          Text(
+            'Sign Up Page Content',
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 20.0),
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: 'Username',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          SizedBox(height: 10.0),
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: 'Email',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          SizedBox(height: 10.0),
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: 'Password',
+              border: OutlineInputBorder(),
+            ),
+            obscureText: true, 
+          ),
+          SizedBox(height: 20.0),
+          ElevatedButton(
+            onPressed: () {
+            
+            },
+            child: Text('Sign Up'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: SignUpPage(),
+    ),
+  );
+}
