d76da1b429d819e580679debf58d8b2b84a725c7
Fatima Grace Apinan <89889380+FreyaGrace@users.noreply.github.com>
2023-10-20 10:56:42 +0800

    pubsec modified and fixed

--- lib/features/sign_up.dart
+++ lib/features/sign_up.dart
@@ -1,158 +1,172 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';//For formatting dates
-//import 'package:bmi_tracker/features/log_in.dart';
+import 'package:intl/intl.dart';
+import 'package:cc206_bmi_tracker/features/log_in.dart';
 
 class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(95, 116, 209, 1),
+      appBar: AppBar(
+          backgroundColor: Colors.transparent, // Set background to transparent
+          elevation: 0,
+      ),
+      body: SignUpForm(),
+    );
+  }
+}
 
-    void _navigateToSignUpPage(BuildContext context) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => LoginPage())
-      );
-   }
-  //For getting input values
+class SignUpForm extends StatefulWidget {
+  @override
+  _SignUpFormState createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  void _navigateToSignUpPage(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => LoginPage()),
+    );
+  }
   final TextEditingController usernameController = TextEditingController(); 
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController dateinput = TextEditingController();
- final String? selectedGender;
- 
-
- SignUpPage({
-    Key? key,
-    this.selectedGender,
-  }) : super(key: key);
-
+  final List<String> genderOptions = ['Male', 'Female',];
+  String selectedGender = 'Male';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI Tracker'),
-      ),
-      body: Column(
-          children: [
-            Text(
-              'Sign Up',
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-              ),
+    return Column(
+      children: [
+         const Text(
+          'Body VitalityQuest',
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+                    Container(
+                      width: 175,
+                      padding: const EdgeInsets.fromLTRB(65, 100, 15, 10),
+                      child: GestureDetector(
+                       onTap: () => _navigateToSignUpPage(context),
+                        child: const Text(
+                       "Log in", textAlign: TextAlign.start,
+                       style: TextStyle(
+                        color: Colors.white,
+                         fontSize: 16,
+                       
+                      ),
+                      ),
+                    ),
+                      
+                    ), //Container
+                    const SizedBox(
+                      width: 20,
+                    ), //SizedBox
+                    Container(
+                        width: 175,
+                        padding: const EdgeInsets.fromLTRB(15, 100, 10, 10),
+                        child: const Text(
+                        'Sign Up',
+                      style: TextStyle(
+                       fontSize: 16,
+                       fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                         ),
+                        ),
+                   ) //Container
+                  ],
+        ),
+          SizedBox(height: 20.0),
+          TextFormField(
+            controller: usernameController,
+            decoration: InputDecoration(
+              labelText: 'Username',
+              border: OutlineInputBorder(),
             ),
-            SizedBox(height: 20.0),
-            TextFormField(
-              controller: usernameController, // Add the controller
-              decoration: InputDecoration(
-                labelText: 'Username',
-                border: OutlineInputBorder(),
-              ),
+          ),
+          SizedBox(height: 10.0),
+          TextFormField(
+            controller: emailController,
+            decoration: InputDecoration(
+              labelText: 'Email',
+              border: OutlineInputBorder(),
             ),
-            SizedBox(height: 10.0),
-            TextFormField(
-              controller: emailController, // Add the controller
-              decoration: InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
-              ),
+          ),
+          SizedBox(height: 10.0),
+          TextFormField(
+            controller: passwordController,
+            decoration: const InputDecoration(
+              labelText: 'Password',
+              border: OutlineInputBorder(),
             ),
-            SizedBox(height: 10.0),
-            TextFormField(
-              controller: passwordController, // Add the controller
-              decoration: InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
-              ),
-              obscureText: true,
-            ),
-            SizedBox(height: 10.0),
-            ListTile(
-              title: Text('Gender'),
-              contentPadding: EdgeInsets.all(0),
-              dense: true,
-              subtitle: Row(
-                children: <Widget>[
-                  Radio<String>(
-                    value: 'Male',
-                    groupValue: selectedGender,
-                    onChanged: null,
-                  ),
-                  Text('Male'),
-                  Radio<String>(
-                    value: 'Female',
-                    groupValue: selectedGender,
-                    onChanged: null,
-                  ),
-                  Text('Female'),
-                ],
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(15), // Sets all sides 10 units
-              height: 150,
-              child: Center(
-                child: TextField(
-                  controller: dateinput,
-                  decoration: InputDecoration(
-                    icon: Icon(Icons.calendar_today),
-                    labelText: "Enter Birthdate",
-                  ),
-                  readOnly: true,
-                  onTap: () async {
-                    DateTime? pickedDate = await showDatePicker(
-                      context: context,
-                      initialDate: DateTime.now(),
-                      firstDate: DateTime(2000),
-                      lastDate: DateTime(2101),
-                    );
-                    if (pickedDate != null) {
-                      String formattedDate = DateFormat('yyyy-MM-dd').format(pickedDate); // Use of intl to format date
-                      dateinput.text = formattedDate;
-                    }
-                  },
+            obscureText: true,
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+            height: 110,
+            child: Center(
+              child: TextField(
+                controller: dateinput,
+                decoration: InputDecoration(
+                  suffixIcon: Icon(Icons.calendar_today),
+                  labelText: "MM/DD/YY",
                 ),
+                readOnly: true,
+                onTap: () async {
+                  DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime(2101),
+                  );
+                  if (pickedDate != null) {
+                    String formattedDate = DateFormat('yyyy-MM-dd').format(pickedDate);
+                    dateinput.text = formattedDate;
+                  }
+                },
               ),
             ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('Sign Up'),
+          ),
+           Center(
+          child: ListTile(
+            contentPadding: const EdgeInsets.all(10),
+            dense: true,
+            subtitle: DropdownButton<String>(
+              value: selectedGender,
+              icon: const Icon(Icons.arrow_drop_down),
+              isExpanded: true,
+              iconSize: 30.2,
+              iconEnabledColor: Color.fromRGBO(2, 0, 88, 1),
+              items: genderOptions.map((String gender) {
+                return DropdownMenuItem<String>(
+                  value: gender,
+                  child: Text(gender),
+                );
+              }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedGender = value!;
+                });
+              },
             ),
-            SizedBox(height: 10),
-            Center(
-  child: GestureDetector(
-    onTap: () => _navigateToSignUpPage(context),
-    child: const Text(
-      "Already have an account? Log in",
-      style: TextStyle(
-        color: Colors.blue,
-        decoration: TextDecoration.underline,
-      ),
-    ),
-  ),
-)
-          ],
+          ),
         ),
-    );
-  }
-}
-class SignUpPageParent extends StatefulWidget {
-  @override
-  _SignUpPageParentState createState() => _SignUpPageParentState();
-}
-
-class _SignUpPageParentState extends State<SignUpPageParent> {
-  String? selectedGender;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI Tracker'),
-      ),
-      body: SingleChildScrollView(
-        child: SignUpPage(
-          selectedGender: selectedGender,
-        ),
-      ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+            primary: const Color.fromRGBO(255, 162, 38, 1) , // Background color
+            ),
+            child: Text('Sign Up', style: TextStyle(fontSize: 20)),
+          ),
+        ]
     );
   }
 }
