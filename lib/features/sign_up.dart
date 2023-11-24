713b2c3ce5c8e1a03597251f2ae48c48e19b373c
Fatima Grace Apinan <89889380+FreyaGrace@users.noreply.github.com>
2023-11-24 17:59:35 +0800

    Sign Up Update

--- lib/features/sign_up.dart
+++ lib/features/sign_up.dart
@@ -1,172 +1,234 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:cc206_bmi_tracker/features/log_in.dart';
+import 'package:cc206_bmi_tracker/features/calculator.dart';
 
-class SignUpPage extends StatelessWidget {
-  const SignUpPage({super.key});
+class SignUpPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor:Color.fromRGBO(95, 116, 209, 1),
-      appBar: AppBar(
-          backgroundColor: Colors.transparent, // Set background to transparent
-          elevation: 0,
-      ),
-      body: SignUpForm(),
-    );
-  }
+  _SignUpPageState createState() => _SignUpPageState();
 }
 
-class SignUpForm extends StatefulWidget {
-  @override
-  _SignUpFormState createState() => _SignUpFormState();
-}
-
-class _SignUpFormState extends State<SignUpForm> {
-  void _navigateToSignUpPage(BuildContext context) {
+class _SignUpPageState extends State<SignUpPage> {
+  void _navigateToLoginPage(BuildContext context) {
     Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => LoginPage()),
     );
   }
-  final TextEditingController usernameController = TextEditingController(); 
+
+  void _navigateToCalculatorPage(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => CalculatorPage()),
+    );
+  }
+
+  final TextEditingController usernameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController dateinput = TextEditingController();
-  final List<String> genderOptions = ['Male', 'Female',];
-  String selectedGender = 'Male';
+  String? selectedGender;
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-         const Text(
-          'Body VitalityQuest',
-          style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
+    return Scaffold(
+      appBar: null,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color.fromARGB(255, 37, 34, 169),
+              Color.fromARGB(255, 130, 148, 252),
+            ],
           ),
         ),
-        Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-                    Container(
-                      width: 175,
-                      padding: const EdgeInsets.fromLTRB(65, 100, 15, 10),
-                      child: GestureDetector(
-                       onTap: () => _navigateToSignUpPage(context),
-                        child: const Text(
-                       "Log in", textAlign: TextAlign.start,
-                       style: TextStyle(
-                        color: Colors.white,
-                         fontSize: 16,
-                       
-                      ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20.0),
+                  child: Text(
+                    'Body VitalityQuest',
+                    style: TextStyle(
+                      fontSize: 31,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextButton(
+                      onPressed: () {
+                        _navigateToLoginPage(context);
+                      },
+                      child: Text(
+                        'Log in',
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                      
-                    ), //Container
-                    const SizedBox(
-                      width: 20,
-                    ), //SizedBox
-                    Container(
-                        width: 175,
-                        padding: const EdgeInsets.fromLTRB(15, 100, 10, 10),
-                        child: const Text(
-                        'Sign Up',
-                      style: TextStyle(
-                       fontSize: 16,
-                       fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                         ),
+                    SizedBox(width: 110),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Sign up',
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
                         ),
-                   ) //Container
+                      ),
+                    ),
                   ],
-        ),
-          SizedBox(height: 20.0),
-          TextFormField(
-            controller: usernameController,
-            decoration: InputDecoration(
-              labelText: 'Username',
-              border: OutlineInputBorder(),
-            ),
-          ),
-          SizedBox(height: 10.0),
-          TextFormField(
-            controller: emailController,
-            decoration: InputDecoration(
-              labelText: 'Email',
-              border: OutlineInputBorder(),
-            ),
-          ),
-          SizedBox(height: 10.0),
-          TextFormField(
-            controller: passwordController,
-            decoration: const InputDecoration(
-              labelText: 'Password',
-              border: OutlineInputBorder(),
-            ),
-            obscureText: true,
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
-            height: 110,
-            child: Center(
-              child: TextField(
-                controller: dateinput,
-                decoration: InputDecoration(
-                  suffixIcon: Icon(Icons.calendar_today),
-                  labelText: "MM/DD/YY",
                 ),
-                readOnly: true,
-                onTap: () async {
-                  DateTime? pickedDate = await showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime(2000),
-                    lastDate: DateTime(2101),
-                  );
-                  if (pickedDate != null) {
-                    String formattedDate = DateFormat('yyyy-MM-dd').format(pickedDate);
-                    dateinput.text = formattedDate;
-                  }
-                },
-              ),
-            ),
-          ),
-           Center(
-          child: ListTile(
-            contentPadding: const EdgeInsets.all(10),
-            dense: true,
-            subtitle: DropdownButton<String>(
-              value: selectedGender,
-              icon: const Icon(Icons.arrow_drop_down),
-              isExpanded: true,
-              iconSize: 30.2,
-              iconEnabledColor: Color.fromRGBO(2, 0, 88, 1),
-              items: genderOptions.map((String gender) {
-                return DropdownMenuItem<String>(
-                  value: gender,
-                  child: Text(gender),
-                );
-              }).toList(),
-              onChanged: (value) {
-                setState(() {
-                  selectedGender = value!;
-                });
-              },
+                SizedBox(height: 20),
+                TextField(
+                  controller: usernameController,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                    labelStyle: TextStyle(color: Colors.white),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  controller: emailController,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    labelStyle: TextStyle(color: Colors.white),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  controller: passwordController,
+                  style: TextStyle(color: Colors.white),
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: TextStyle(color: Colors.white),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  child: Center(
+                    child: TextField(
+                      controller: dateinput,
+                      style: TextStyle(color: Colors.white),
+                      textAlign: TextAlign.left, // Set to TextAlign.left
+                      decoration: InputDecoration(
+                        labelText: 'Date of Birth',
+                        labelStyle: TextStyle(color: Colors.white),
+                        contentPadding:
+                            EdgeInsets.only(bottom: 10), // Adjust as needed
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        suffixIcon: Padding(
+                          padding: const EdgeInsets.only(left: 8.0),
+                          child:
+                              Icon(Icons.calendar_today, color: Colors.white),
+                        ),
+                      ),
+                      readOnly: true,
+                      onTap: () async {
+                        DateTime? pickedDate = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2000),
+                          lastDate: DateTime(2101),
+                        );
+                        if (pickedDate != null) {
+                          String formattedDate =
+                              DateFormat('yyyy-MM-dd').format(pickedDate);
+                          dateinput.text = formattedDate;
+                        }
+                      },
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                DropdownButtonFormField<String>(
+                  value: selectedGender,
+                  onChanged: (value) {
+                    setState(() {
+                      selectedGender = value;
+                    });
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Gender',
+                    labelStyle: TextStyle(color: Colors.white),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                  items: ['Male', 'Female', 'Other']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(
+                        value,
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    );
+                  }).toList(),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    _navigateToCalculatorPage(context);
+                  },
+                  child: Text('SIGN UP'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.orange,
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-            primary: const Color.fromRGBO(255, 162, 38, 1) , // Background color
-            ),
-            child: Text('Sign Up', style: TextStyle(fontSize: 20)),
-          ),
-        ]
+      ),
+    );
+  }
+}
+
+class SignUpPageParent extends StatefulWidget {
+  @override
+  _SignUpPageParentState createState() => _SignUpPageParentState();
+}
+
+class _SignUpPageParentState extends State<SignUpPageParent> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI Tracker'),
+      ),
+      body: SingleChildScrollView(
+        child: SignUpPage(),
+      ),
     );
   }
 }
@@ -174,7 +236,7 @@ class _SignUpFormState extends State<SignUpForm> {
 void main() {
   runApp(
     MaterialApp(
-      home: SignUpPage(),
+      home: SignUpPageParent(),
     ),
   );
 }
