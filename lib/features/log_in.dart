15124f75c30e1262556b9d064360e0c1bdd198ab
SarahCalinagan <sarahnicole.calinagan@wvsu.edu.ph>
2023-11-24 15:12:08 +0800

    unit 6-7

--- lib/features/log_in.dart
+++ lib/features/log_in.dart
@@ -2,10 +2,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:cc206_bmi_tracker/features/sign_up.dart';
+import 'package:cc206_bmi_tracker/features/calculator.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
 
+class _LoginPageState extends State<LoginPage> {
   void _navigateToSignUpPage(BuildContext context) {
     Navigator.push(
       context,
@@ -13,64 +17,116 @@ class LoginPage extends StatelessWidget {
     );
   }
 
+  void _navigateToCalculatorPage(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => CalculatorPage()),
+    );
+  }
+
+  final TextEditingController usernameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI Tracker'),
-      ),
-      body: Column(
-        children: [
-          Text(
-            'Login',
-            style: TextStyle(
-              fontSize: 24.0,
-              fontWeight: FontWeight.bold,
+      appBar: null,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color.fromARGB(255, 37, 34, 169),
+              Color.fromARGB(255, 130, 148, 252)
+            ],
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20.0),
+                  child: Text(
+                    'Body VitalityQuest',
+                    style: TextStyle(
+                      fontSize: 31,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Log in',
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 110),
+                    TextButton(
+                      onPressed: () {
+                        _navigateToSignUpPage(context);
+                      },
+                      child: Text(
+                        'Sign up',
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  controller: usernameController,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                    labelStyle: TextStyle(color: Colors.white),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  style:
+                      TextStyle(color: Colors.white), // Set text color to white
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: TextStyle(color: Colors.white),
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)),
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    _navigateToCalculatorPage(context);
+                  },
+                  child: Text('LOGIN'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.orange,
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                  ),
+                ),
+              ],
             ),
           ),
-          SizedBox(height: 20.0),
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: 'Username',
-              border: OutlineInputBorder(),
-            ),
-          ),
-          SizedBox(height: 10.0),
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: 'Password',
-              border: OutlineInputBorder(),
-            ),
-            obscureText: true,
-          ),
-          SizedBox(height: 20.0),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text('Log in'),
-          ),
-          SizedBox(height: 10),
-          GestureDetector(
-            onTap: () => _navigateToSignUpPage(context),
-            child: const Text(
-              "Don't have an account? Sign up",
-              style: TextStyle(
-                color: Colors.blue,
-                decoration: TextDecoration.underline,
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
 }
-
-void main() {
-  runApp(
-    MaterialApp(
-      home: LoginPage(),
-    ),
-  );
-}
-
-//ayy
